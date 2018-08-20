8cb1146b0908ae00fd42cd608d1f6c10c366ca26
Mona <bocharova.mona@gmail.com>
2018-08-20 11:02:17 +0100

    make guest username clickable

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-    var monthsList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Aug', 'Oct', 'Nov', 'Dec'];
+    var guestTweets = [];
     var dataArrLength = streams.home.length;
     var index = streams.home.length - 1;
-    while (index >= 0) {
-        var initialTweets = getTweet(streams.home[index], streams.home[index].user);
+    while (index >= 0) {                                 // loads initial tweets when page is refreshed
+        var initialTweets = getTweet(streams.home[index]);
         initialTweets.appendTo("#tweet-section");
         index -= 1;
     }
-    setInterval(function () {
+    setInterval(function () {                           //loads newly generated tweets every second
         if (streams.home.length > dataArrLength) {
             var numberOfNewTweets = streams.home.length - dataArrLength;
             var start = dataArrLength;
             for (var i = 0; i < numberOfNewTweets; i++) {
-                var newTweet = getTweet(streams.home[start], streams.home[start].user);
+                var newTweet = getTweet(streams.home[start]);
                 newTweet.prependTo("#tweet-section:first-child");
                 start++;
             }
@@ -21,47 +21,56 @@ $(document).ready(function () {
         $('.time').each(function () {
             $(this).html(moment($(this).attr('data')).from(Date.now()));
         });
-    }, 30000);
+    }, 1000);
 
-    function getTweet(tweet, user) {
+    function getTweet(tweet) {                          // formats tweets
         var $tweet = $('<p></p>');
+        var user = tweet.user;
         var tweetTime = tweet.created_at;
         var link = '<a class="link" href = #>' + user + '</a>';
         var formattedTime = moment(tweetTime).from(Date.now());
-        $tweet.html('@' + link + ' . ' + '<span class="time" data="'+tweetTime+'">'+formattedTime+'</span>' + ' : ' + tweet.message);
+        $tweet.html('@' + link + ' . ' + '<span class="time" data="' + tweetTime + '">' + formattedTime + '</span>' + ' : ' + tweet.message);
         return $tweet;
     }
 
-    $('#tweet-section').on('click', 'a', function (event) {
+    $('#tweet-section').on('click', 'a', function (e) {     //  handles username click event to filter tweets for that username
         $('#tweet-section').addClass('hide');
         $('#tweet-area').addClass('hide');
         $('#timeline-section').removeClass('hide');
         $('#timeline-area').removeClass('hide');
         var userName = this.text;
         var searchArr = streams.home;
+        if (userName === 'guestUser') {
+            searchArr = guestTweets;
+        }
         var startingPoint = 0;
         var i = startingPoint;
         setInterval(function () {
             var length = searchArr.length;
             for (i = startingPoint; i < length; i++) {
                 if (searchArr[i].user === userName) {
-                    var userTweets = getTweet(searchArr[i], userName);
+                    var userTweets = getTweet(searchArr[i]);
                     userTweets.prependTo("#timeline-section:first-child");
                 }
             }
             startingPoint = length;
         }, 100);
-        event.preventDefault();
+        e.preventDefault();
         return false;
     });
 
-    $('#tweet-button').click(function () {
-        var tweet = $('#user-tweet').val();
-        var $tweet = $('<p></p>');
-        var tweetTime = new Date();
-        var formattedTime = moment(tweetTime).from(Date.now());
-        $tweet.html('@guestUser' + ' . ' + '<span class="time" data="'+tweetTime+'">'+formattedTime+'</span>' + ' : ' + tweet);
-        $tweet.prependTo("#tweet-section:first-child");
+    $('#tweet-button').click(function () {                   // handles guest user tweet click event which lets user tweet
+        var message = $('#user-tweet').val();
+        if (message.length > 0) {
+            var tweet = {};
+            tweet.user = 'guestUser';
+            tweet.message = message;
+            tweet.created_at = new Date();
+            guestTweets.push(tweet);
+            var userTweet = getTweet(tweet);
+            userTweet.prependTo("#tweet-section:first-child");
+            $('#user-tweet').val('');
+        }
     })
 
 });
